1983d57d7500826421436d10cb6574a3448d591b
Dylan Gebl <dylanfoxx@optonline.net>
2017-10-22 14:39:11 -0400

    Jackyoushit

--- JS4.c
+++ JS4.c
@@ -3,12 +3,25 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define NAMELENGTH 32
+#define MAXFILESEC 64
 extern int dinit();
 extern int rsector(int t,int s,unsigned char *b);
 extern int wsector(int t,int s,unsigned char *b);
 
+struct __SectorTrack{
+    unsigned int sector;
+    unsigned int track;
+};
+
+struct __LSectorTrack{
+    struct __SectorTrack *ST;
+    struct __LSectorTrack *next;
+    struct __LSectorTrack *prev;
+};
+
 struct __meta_file{
-    char file_name[32];
+    char *file_name;
     unsigned int file_size_byte;
     time_t file_create_time;
     time_t last_read_time;
@@ -16,37 +29,45 @@ struct __meta_file{
 };
 
 struct __open_file_info{
-    char RWO;
-    int fd;
-    int s_sector;
-    int s_track;
+    char RW;//if its for read write
+    int ST; //how many blocks we take up
+    int fd; //its identifier
+    struct __LSectorTrack *start; //the start of the list of blocks we take up
 };
 
-struct __file{
+struct __file{ //holds meta data and info about openfile
     struct __meta_file  meta_file;
     struct __open_file_info open_file_info;
 };
 
-struct __Lfile{
+struct __Lfile{ //linked list for files
     struct __file *file;
-    struct __Lfile *prev;
     struct __Lfile *next;
+    struct __Lfile *prev;
 };
 
-int finit(){
+int finit(){ //inits file system
     //read the disk meta data in
     printf("Size of __file %d\n",sizeof(struct __file));
     printf("Size of __Lfile %d\n",sizeof(struct __Lfile));
 }
 
 int dopen(char *fname, char *mode){
-    if(strlen(fname) > 128){
+
+    int fle
+    __Lfile tmp = __isfile(fname,&fle);
+
+    if(fle == -1){
+        printf("File name to long\n");
         return -1;
     }
 
-    if(mode[0] == 'w'){//
-
-    }else if(mode[0] == 'r'){
+    if(mode[0] == 'w' && fle == 1){//file exists no need to create it
+        
+    }else if (mode[0] == 'w' && fle == -2){//create file
+        struct __file *tmp =  __addFile(fname);
+        //set mode and give fd
+    }else if(mode[0] == 'r' && fle == 1){//file exists and its good to read
 
     }else{
         return -1;
@@ -55,18 +76,22 @@ int dopen(char *fname, char *mode){
 }
 
 struct __file* __getFile(char *fname){
-    
+    if(strlen(fname) > 32){
+        return -1;
+    }
 }
 
 struct __Lfile *start = NULL;
+int __NumFiles = 0;
 
-struct __file* __addFile(){//adds file to linked list of files
+struct __file* __addFile(char *fname){//adds file to linked list of files
     struct __Lfile *tmp = malloc(sizeof(struct __Lfile));
     tmp->file->meta_file.file_create_time = time(NULL);
     tmp->file->meta_file.last_read_time = time(NULL);
     tmp->file->meta_file.last_write_time = time(NULL);
     tmp->prev = NULL;
     tmp->next = NULL;
+    tmp->file->meta_file.name = strdup(fname);
 
     if(start == NULL){
         start = tmp;
@@ -79,18 +104,116 @@ struct __file* __addFile(){//adds file to linked list of files
         tmp2->next = tmp;
         tmp->prev = tmp2;
     }
+
+    __NumFiles++;
+    return tmp;
 }
 
-int __delFile(char *fname){
-    struct __Lfile *tmp;   
-    
+int highestFD fd;
+struct FDLIST *returnedfdList;
+
+struct FDLIST{
+    int fd;
+    struct FDLIST *next;
+    struct FDLIST *prev;
+};
+
+
+int __nextFD(){
+
+}
+
+int dclose(int fd){
+    if(fd > highestFD){
+        return -1;
+    }
+}
+
+struct __Lfile* __isfile(char *fname,int *ret){//-1 fname to long, -2 file does not exist,1 file exists
+    if(strlen(fname) > NAMELENGTH){
+        *ret = -1;
+        return NULL;
+    }
+
+    if(start == NULL){
+        *ret = -2;
+        return NULL;
+    }
+
+    struct __Lfile *tmp;
     for(tmp = start;tmp->next;tmp = tmp->next){
+        if(strcmp(tmp->file->meta_file.file_name,fname) == 0){
+            *ret = 1
+            return tmp;
+        }
+    }
+
+    if(strcmp(tmp->file->meta_file.file_name,fname) == 0){
+        *ret = 1
+        return tmp;
+    }
+
+    *ret = -2;
+    return NULL;
+}
+
+int dunlink(char *fname){
+    
+    struct __Lfile *tmp;  
+    int ret;
+    
+    tmp = __isfile(fname,&ret);
+
+    if(tmp != NULL){
+        if(tmp == start){
+            if(tmp->next){
+                start = next;
+            }else{
+                start = NULL;
+            }
+        }else{
+            if(tmp->next){
+                tmp->prev->next = tmp->next;
+                if(__freeFile(tmp) > 0){
+                    return 1;
+                }else{
+                    return 0;
+                }
+            }else{
+                tmp->prev->next = NULL;
+
+            }
+        }
+    }else{
+        return 0;
+    }
+
+
+    int __freeFile(struct __Lfile *file,struct __Lfile *prev,struct __Lfile*next){//returns <= 1 on error or 1 on success
+        if(prev == NULL && next == NULL){
+            START == NULL;
+        }else if(prev == NULL){
+            start = file->next;
+        }else if(next == NULL){
+            prev->next = NULL;
+        }else{
+            prev->next = next;
+            next->prev = prev;
+        }
+        free(file->file->meta_file->filename);//name the only thing in meta that needs to be freed
+        free(file->file->meta_file);//we can free meta file now
+
+        num_fields--;
+    }
+
+    /*for(tmp = start;tmp->next;tmp = tmp->next){
         if(strcmp(tmp->file->meta_file.file_name,fname) == 0){
             tmp->prev->next = tmp->next;
             if(tmp->next){
                 tmp->next->prev = tmp->prev;
             }
             free(tmp);
+            __NumFiles--;
             return 1;
         }
     }
@@ -102,11 +225,17 @@ int __delFile(char *fname){
         }
         free(tmp);
         return 1;
-    }
+    }*/
 
-    return 0;
     
+    return 0;
 }
+
+int writeFileMetatoDisk(){
+
+}
+
+
 
 
 //open a file for reading or writing
@@ -115,8 +244,7 @@ int __delFile(char *fname){
 
 
 //int dclose(int fd)
-//close a file == file descriptor is no longer valid and may be reused
-
+//close a file == file descriptor is no longer
 
 //int dread(int fd, char *buf)
 //read the next sector-size chunk of bytes from the file and store it in buf
